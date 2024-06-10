8ead273e356a86f6400c6897662ba56cbc10d49e
NvDzung <dung3032003md@gmail.com>
2024-06-10 17:14:22 +0700

    Using Stream API

--- src/main/java/passing_code/Main.java
+++ src/main/java/passing_code/Main.java
@@ -2,6 +2,8 @@ package passing_code;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
@@ -9,6 +11,10 @@ public class Main {
         // Có thể tryền một phương thức vào làm tham số (parameter).
         List<Apple> result = FilterApple.filterApples(inventory.getListApples(), (Apple a) -> AppleColor.RED.name().equals(a.getColor()));
         System.out.println(result);
+
+        // Using Stream API
+        List<Apple> listGreenApples = inventory.getListApples().stream().filter((Apple a) -> a.getColor().equals(AppleColor.GREEN.name())).collect(toList());
+        System.out.println(listGreenApples);
     }
 }
 
