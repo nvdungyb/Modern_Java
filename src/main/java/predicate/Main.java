b1611342e5c1338bbdd0883e5cd0591678cc0afe
NvDzung <dung3032003md@gmail.com>
2024-06-24 17:16:35 +0700

    Composing Predicates

---
+++ src/main/java/predicate/Main.java
@@ -0,0 +1,33 @@
+package predicate;
+
+import passing_code.Apple;
+import passing_code.AppleColor;
+import passing_code.Inventory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static List<Apple> getListApples(Predicate predicate) {
+        Inventory inventory = new Inventory();
+
+        List<Apple> listApples = inventory.getListApples();
+        List<Apple> res = new ArrayList<>();
+        for (Apple apple : listApples) {
+            if (predicate.test(apple))
+                res.add(apple);
+        }
+        return res;
+    }
+
+    // Composing Predicates: read more at java.util.function.Predicate;
+    public static void main(String[] args) {
+        Predicate<Apple> redApple = (Apple apple) -> apple.getColor().equals(AppleColor.RED.name());
+        Predicate<Apple> redAndHeavyApple = redApple.and((Apple apple) -> apple.getWeight() > 150);
+
+        List<Apple> result = getListApples(redAndHeavyApple);
+        System.out.println(result);
+    }
+}
