8d56767068298e04c82506946ecd50af640bc88e
NvDzung <dung3032003md@gmail.com>
2024-06-24 15:48:28 +0700

    Composing Comparators

--- src/main/java/passing_code/Main.java
+++ src/main/java/passing_code/Main.java
@@ -1,30 +1,14 @@
 package passing_code;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
 
-//        // Có thể tryền một phương thức vào làm tham số (parameter).
-//        List<Apple> result = FilterApple.filterApples(inventory.getListApples(), (Apple a) -> AppleColor.RED.name().equals(a.getColor()));
-//        System.out.println(result);
-
-        // Using anonymous class
-        List<Apple> result = Filter.filterProduct(inventory.getListApples(), new Predicate<Apple>() {
-            public boolean test(Apple apple) {
-                if (apple.getColor().equals(AppleColor.RED.name()) && apple.getWeight() > 200)
-                    return true;
-                return false;
-            }
-        });
-
-        System.out.println(result);
-
+        List<Apple> listApples = inventory.getListApples();
+        listApples.sort(Comparator.comparingInt(Apple::getWeight).reversed().thenComparing(Apple::getColor));
+        System.out.println(listApples);
     }
 }
-
-/* Tại sao có thể truyền các method reference và lambda expression vào các phương thức mà chấp nhận functional interface như là tham số.
-    - Khi ta truyền một method reference hoặc một lambda expression, Java sẽ tự động chuyển đổi nó thành một instance của một functional interface tương ứng.
-
- */
