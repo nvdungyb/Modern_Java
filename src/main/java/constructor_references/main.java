65d203ff7ecf33f955cd44a88cf60d505b9838c6
NvDzung <dung3032003md@gmail.com>
2024-06-24 14:39:40 +0700

    Simple case make interesting application

--- src/main/java/constructor_references/main.java
+++ src/main/java/constructor_references/main.java
@@ -3,12 +3,27 @@ package constructor_references;
 import passing_code.Apple;
 import passing_code.AppleColor;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 public class main {
+
+    public static Map<String, BiFunction<Integer, AppleColor, Apple>> map = new HashMap<>();
+
+    public static void init() {
+        map.put("apple", Apple::new);
+//        map.put("orange", Orange::new);
+    }
+
+    public static Apple giveFruit(String fruit, Integer weight, AppleColor appleColor) {
+        return map.get(fruit.toLowerCase()).apply(weight, appleColor);
+    }
+
     public static void main(String[] args) {
-        BiFunction<Integer, AppleColor, Apple> func = Apple::new;
-        Apple res = func.apply(100, AppleColor.RED);
+        init();
+        Apple res = giveFruit("Apple", 200, AppleColor.GREEN);
         System.out.println(res);
     }
 }
